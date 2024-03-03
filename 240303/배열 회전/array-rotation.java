89ac767e688bd41eb40e4b826ae1a6700538e369
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-03-03 17:04:58 +0900

    Title: 배열 회전, Time: 817ms, Memory: 26MB, Status: Passed - CodeTree

--- 240303/배열 회전/array-rotation.java
+++ 240303/배열 회전/array-rotation.java
@@ -9,102 +9,42 @@ public class Main {
     static int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
     static int [][] map;
 
-
-    static void input() throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        
-
+    public static void rotate() {
         depthSize = Math.min(N, M) / 2;
-        layers = new ArrayList[depthSize];
-        for(int i = 0; i <depthSize; i++) layers[i]= new ArrayList<>();
-        map = new int[N][M];
-        for(int i = 0; i< N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j <M; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+
+        for(int dpt = 0; dpt < depthSize; dpt++){
+            int n_max = N - dpt - 1;
+            int m_max = M - dpt - 1;
+
+            //레이어 위쪽 상단 값 임시 저장
+            int tmp = map[dpt][dpt];
+
+            //  << 이동
+            for(int c = dpt+1; c <= m_max; c++){
+                map[dpt][c-1] = map[dpt][c];
             }
+
+            // 위쪽 이동
+            for(int r = dpt + 1; r <=n_max; r++){
+                map[r-1][m_max] = map[r][m_max];
+            }
+
+            // >> 이동
+            for(int c = m_max -1; c >= dpt; c--){
+                map[n_max][c+1] = map[n_max][c];
+            }
+
+            // 아래쪽 이동
+            for(int r = n_max-1; r >= dpt; r--){
+                map[r+1][dpt] = map[r][dpt];
+            }
+
+            map[dpt+1][dpt] = tmp;
+
         }
-        
-        for(int d = 0; d <depthSize; d++){
-
-            int h = d != 0 ? N / (2 * d) : N;
-            // if( d != 0 &&  (N & 1) == 1) h += 1;
-            int w = d != 0 ? M / (2 * d): M;
-            // if( d != 0 &&  (M & 1) == 1) w += 1;
-            // System.out.println("h = "+ h + " w = "+ w);
-
-            for(int down = 0; down < h -1 ; down++){
-                int r = d + dirs[0][0] * down;
-                int c = d + dirs[0][1] * down;
-                layers[d].add(map[r][c]);
-            }
-            for(int right = 0; right < w -1 ; right++){
-                int r = d + h -1 + dirs[1][0] * right;
-                int c = d + dirs[1][1] * right;
-                layers[d].add(map[r][c]);
-            }
-            for(int up = 0; up < h -1 ; up++){
-                int r = d + h - 1 + dirs[2][0] * up;
-                int c = d + w - 1 + dirs[2][1] * up;
-                layers[d].add(map[r][c]);
-            }
-            for(int left = 0; left < w -1 ; left++){
-                int r = d + dirs[3][0] * left;
-                int c = d + w - 1 + dirs[3][1] * left;
-                layers[d].add(map[r][c]);
-            }
-        }
-        // System.out.println(Arrays.toString(layers));
-        
+        // printMap(map);
     }
-    public static void sol() {
-        
-        int [][] res = new int[N][M];
-        for(int i = 0; i< N; i++) res[i] = map[i].clone();
 
-        for(int d = 0; d <depthSize; d++){
-            if(layers[d].size() == 0) break;
-            int idxL = layers[d].size() - 1 - (K % layers[d].size());
-            // System.out.println("idxL = "+ idxL);
-
-            int h =  h = d != 0 ? N / (2 * d): N;
-            // if( d != 0 &&  (N & 1) == 1) h += 1;
-            int w =  w = d != 0 ? M / (2 * d) : M;
-            // if( d != 0 &&  (M & 1) == 1) w += 1;
-
-            for(int down = 0; down < h -1 ; down++){
-                int r = d + dirs[0][0] * down;
-                int c = d + dirs[0][1] * down;
-                idxL = nextPos(idxL, layers[d].size());
-                res[r][c] = layers[d].get(idxL);
-            }
-            for(int right = 0; right < w -1 ; right++){
-                int r = d + h -1 + dirs[1][0] * right;
-                int c = d + dirs[1][1] * right;
-                idxL = nextPos(idxL, layers[d].size());
-                res[r][c] = layers[d].get(idxL);
-            }
-            for(int up = 0; up < h -1 ; up++){
-                int r = d + h - 1 + dirs[2][0] * up;
-                int c = d + w - 1 + dirs[2][1] * up;
-                idxL = nextPos(idxL, layers[d].size());
-                res[r][c] = layers[d].get(idxL);
-            }
-            for(int left = 0; left < w -1 ; left++){
-                int r = d + dirs[3][0] * left;
-                int c = d + w - 1 + dirs[3][1] * left;
-                idxL = nextPos(idxL, layers[d].size());
-                res[r][c] = layers[d].get(idxL);
-            }
-        }
-        printMap(res);
-    }
-    static int nextPos(int k, int len){
-        return k + 1 < len ? k+1 : 0;
-    }
     static void printMap(int [][] map){
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i< map.length; i++){
@@ -114,12 +54,25 @@ public class Main {
             sb.append("\n");
         }
         System.out.println(sb);
-
     }
     public static void main(String[] args) throws IOException{
         // 여기에 코드를 작성해주세요.
-        input();
-        sol();
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        for(int i = 0; i< N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j <M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < K; i++) rotate();
+       
+        printMap(map);
 
     }
 }
