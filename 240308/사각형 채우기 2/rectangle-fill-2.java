471c30020d6fe8abc4f0a2eac1001cb472a2b108
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-03-08 17:25:37 +0900

    Title: 사각형 채우기 2, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240308/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        /*
+        if n = 3
+        (1열을 만드는 경우의 수 + n-1 길이의 경우의 수) + 2열을 만들 수 있는 경우의 수 + n-2 경우의 수
+        */
+        int N = Integer.parseInt(br.readLine());
+        int [] dp = new int[1000+1];
+        dp[1] = 1;
+        dp[2] = 3;
+        for(int i = 3; i <=N ; i++){
+            dp[i] = dp[i-1] + 3 * dp[i-2];
+        }
+        System.out.println(dp[N]);
+
+    }
+}
