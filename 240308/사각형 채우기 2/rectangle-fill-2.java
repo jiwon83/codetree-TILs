2d2c59087febe4aeef0c8807b8fb75d3736c5b44
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-03-08 17:52:09 +0900

    Title: 사각형 채우기 2, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

--- 240308/사각형 채우기 2/rectangle-fill-2.java
+++ 240308/사각형 채우기 2/rectangle-fill-2.java
@@ -13,7 +13,7 @@ public class Main {
         dp[1] = 1;
         dp[2] = 3;
         for(int i = 3; i <=N ; i++){
-            dp[i] = ( dp[i-1] + (2 * dp[i-2]) % 10007 ) % 10007;
+            dp[i] = ( dp[i-1] + 2 * dp[i-2] ) % 10007;
         }
         // System.out.println(Arrays.toString(dp));
         System.out.println(dp[N]);
