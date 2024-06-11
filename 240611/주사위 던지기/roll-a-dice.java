e29b3a71fae15eabb3c88620fa75ddece997d29d
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-06-11 21:10:35 +0900

    Title: 주사위 던지기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240611/주사위 던지기/roll-a-dice.java
+++ 240611/주사위 던지기/roll-a-dice.java
@@ -1,12 +1,13 @@
 import java.util.*;
 import java.io.*;
 public class Main {
+
     static int N, M, nowR, nowC; // M은 횟수
     static int [][] map;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int [] cube = {4,6,3,1,5,2}; //주사위 정보
+    static int [] cube = new int[]{2, 1, 3}; //주사위 정보 0 = front, 1 = up, 2 = right;
 
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
@@ -35,94 +36,58 @@ public class Main {
         }
         return num;
     }
-    static boolean nextIsOutOfArea(char cmd, int r, int c){
-        int nextR = -1, nextC = -1;
+    static Point getNextPos(char cmd, int r, int c){       
         if(cmd=='L'){
-            nextR = r + dirs[0][0];
-            nextC = c + dirs[0][1];
+            r += dirs[0][0];
+            c += dirs[0][1];
         }
         if(cmd == 'R'){
-            nextR = r + dirs[1][0];
+            r += dirs[1][0];
             nextC = c + dirs[1][1];
         }
         if(cmd == 'U'){
-            nextR = r + dirs[2][0];
-            nextC = c + dirs[2][1];
+            r += dirs[2][0];
+            c += dirs[2][1];
         }
         if(cmd == 'D'){
-            nextR = r + dirs[3][0];
-            nextC = c + dirs[3][1];
+            r += dirs[3][0];
+            c += dirs[3][1];
         }
-        return !inArea(nextR, nextC);
+    }
+    static boolean nextIsOutOfArea(char cmd, int r, int c){
+        Point nextP = getNextPos(cmd, r, c);
+        return !inArea(nextP.x, nextP.y);
     }
     static boolean inArea(int r, int c){
         return r > 0 && c > 0 && r <=N && c <=N;
     }
     static int [][] dirs = {{0,-1}, {0,1}, {-1,0}, {1,0}};
     static void doGame(char cmd){
-        // 주사위를 굴리고
-        switch(cmd){
-            case 'L':
-            nowR = nowR + dirs[0][0];
-            nowC = nowC + dirs[0][1];
-            cube = moveL(cube);
-            break;
-            case 'R':
-            nowR = nowR + dirs[1][0];
-            nowC = nowC + dirs[1][1];
-            cube = moveR(cube);
-            break;
-            case 'U':
-            nowR = nowR + dirs[2][0];
-            nowC += dirs[2][1];
-            cube = moveU(cube);
-            break;
-            case 'D':
-            nowR += dirs[3][0];
-            nowC += dirs[3][1];
-            cube = moveD(cube);
+        Point nextPos =getNextPos(cmd, nowR, nowC);
+        cube = moveCube(cmd, cube);  // 주사위를 굴리고
+        map[nowR][nowC] = cube[1]; // 바닥면을 세긴다.
+
+    }
+    static int [] moveCube(char cmd, int [] cube){
+        int newCube [] = cube.clone();
+        if(cmd == 'U'){
+            newCube[0] = 7-cube[1];
+            newCube[1] = cube[0];
         }
-
-        // 바닥면을 세긴다.
-        map[nowR][nowC] = cube[1];
-
-    }
-    // 0~3까지에 대해 +1 만큼 이동
-    static int [] moveL(int [] cube){
-        int [] tmp = cube.clone();
-        for(int i = 0;i < 4; i++){
-            int ni = (i+1) % 4;
-            tmp[ni] = cube[i];
+        if(cmd == 'D'){
+            newCube[0] = cube[1];
+            newCube[1] = 7-cube[0];
         }
-        return tmp;
-    }
-
-    static int [] moveR(int [] cube){
-        int [] tmp = cube.clone();
-        for(int i = 0;i < 4; i++){
-            int ni = (i-1) >=0 ? i-1 : 3;
-            tmp[ni] = cube[i];
+        if(cmd == 'L'){
+            newCube[1] = cube[2];
+            newCube[2] = 7-cube[0];
         }
-        return tmp;
+        if(cmd == 'R'){
+            newCube[1] = 7-cube[2];
+            newCube[2] = cube[1];
+        }
     }
-
-    static int [] moveD(int [] cube){
-        int [] tmp = cube.clone();
-        tmp[1] = cube[5];
-        tmp[3] = cube[4];
-        tmp[5] = cube[3];
-        tmp[4] = cube[1];
-        return tmp;
-    }
-
-    static int [] moveU(int [] cube){
-        int [] tmp = cube.clone();
-        tmp[1] = cube[4];
-        tmp[3] = cube[5];
-        tmp[4] = cube[3];
-        tmp[5] = cube[1];
-        return tmp;
-    }
+ 
     static int toInt(String s){
         return Integer.parseInt(s);
     }
