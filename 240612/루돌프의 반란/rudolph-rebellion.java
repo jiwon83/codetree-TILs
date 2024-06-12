8aca1221c84f1b2df22852f91e3ef0466a102221
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-06-12 12:53:19 +0900

    Title: 루돌프의 반란, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240612/루돌프의 반란/rudolph-rebellion.java
+++ 240612/루돌프의 반란/rudolph-rebellion.java
@@ -1,210 +1,311 @@
-import java.util.*;
 import java.io.*;
-import java.awt.*;
-/*
-need
-- N*N 
-- for(M개의 턴) 루톨프 1번 움직이고 2. 1번 산타 ~ P번 산타
-
-- 루톨프의 움직임
-    - 가장 가까운 산타를 향해 1칸 이동(게임에서 탈락하지 않은 산타, 기절 산타 상관 없음)
-    - 우선순위 산타 선정 : 거리, r 좌표 내림차순, c 좌표 내림차순
-    - 우선순위 산타에 대해 8방향 중 가장 가까워지는 방향 선택 후, 1칸 이동
-    - if( 산타와 루돌프 같은 칸) -> 충돌
-
-- 산타의 움직임
-    - 1번부터 ~ P번까지 순서대로
-    - 기절해있거나 격자 밖으로 빠져나가 게임에서 탈락한 산타들은 움직이지 않는다. 
-    - 게임 밖 불가, 다른 산타 있는 칸 불가
-    - 우선 순위 방향 선정 : 4방향 중 거리, 상우하좌  -> 우선 탐색
-    - 현재 루돌프와 산타의 거리 
-    - 1 이동 시, 루돌프로부터 가장 가까워지는 방향을 찾는다. 이동시 루돌프와 산타의 거리 (항상 가까워 질때만 갱신)
-    - if ( 못찾았다면 ) 움직이지 않는다. 
-    - if( 산타와 루돌프 같은 칸) -> 충돌
-    
-- 충돌(충돌한 사람, 충돌한 방향)
-    - 산타와 루돌프가 같은 칸이라면  충돌 발생
-    - if( 루돌프의 충돌) 산타.점수 += C;, 루돌프 방향으로 + C칸 이동
-    - if( 산타의 충돌 ) 산타.점수 += D;, 자신의 반대 방향으로 + D칸 이동
-    - if( 밀려난 위치가 게임 밖) 산타 탈락 return
-    - if( 밀려난 칸에 다른 산타 존재) 상호작용 발생, 
-    - 산타의 위치 변경
-    - 산타는 기절
-
-- 상호작용(산타 정보, 착지하는 칸, 방향)
-    - 착지 되는 칸에 다른 산타가 있다면, -> 원래 있던 산타는 방향으로 이동
-    - 연쇄적으로 작용
-    - if( 밀려난 위치가 범위 밖) 산타 탈락
-
-- 기절
-    - 산타는 루돌프와의 충돌 후 기절
-    - 현재 턴 + 1 번 턴까지 기절해있음, 기절 카운드로 적용할 수 있을 듯
-    - 기절 도중 충돌 ok, 상호작용 ok 
-
-- 게임 종료
-    - for (~M)
-        if(모든 산타가 탈락했다면 )-> 즉시 종료
-        아직 탈락하지 않은 산타들에게 1점 점수 부여
-        기절 점수 -1점 감점
-    - 최종 점수 출력
-
-
-*/
+import java.util.*;
+// 1. struct // 2. main(test) + sout ...+ methods(sample) // debug 
 public class Main {
-    static int N, M, P, C, D;
-    static int rouR, rouC;
 
     static class Santa implements Comparable<Santa>{
         int num, r, c, score;
         int sleep;
-        boolean isFail = false;
+        boolean is_out;
         public Santa(int num, int r, int c){
             this.num = num;
             this.r = r;
             this.c = c;
-        }
-        public void startSleep(){
-            this.sleep = 2;
-        }
-        public boolean isFail(){
-            return isFail;
-        }
-        public boolean isSleeping(){
-            return sleep > 0;
+            is_out = false;
         }
         public int compareTo(Santa other){
-            if(this.r > other.r) return -1;
-            else if(this.r == other.r){
-                if(this.c > other.c) return -1;
-                else return 0;
-            }else{
-                return 1;
+            if(this.r == other.r){
+                return Integer.compare(other.c, this.c);
             }
+            return Integer.compare(other.r, this.r);
         }
-        public String toString() {
-            return "산타 번호: "+ num + " isFail : "+isFail +" ( " + r +" , "+ c +" ) "+ " 남은 기절 수 : "+ sleep;
+        public String toString(){
+            return num+"번 산타: ("+ r + " , "+ c + " ) "+ "score = " + score
+                + " sleep = "+ sleep + " is_out = "+ is_out;
         }
 
     }
-    
-    static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static HashMap<Number, Santa> santaHMap = new HashMap<>();
-    static int [][] rouDirs = {{1,0},{-1,0}, {0,-1}, {0,1}, {-1,1}, {1,1}, {-1,-1}, {1,-1}};
-    static int [][] santaDirs = {{-1,0}, {0, 1}, {1,0}, {-1,0}};
-    
+    static int N, M, P, C, D; // p = 산타의 수 
+    static int rouR, rouC; // roudolf 위치
+    static HashMap<Integer, Santa> santaMap = new HashMap<>(); // 번호 별 산타들의 정보
+    static StringBuilder sb = new StringBuilder();
+    static int [][] santaNumMap; // 산타의 번호 map
+    static int [][] dirs8 = {{-1,-1}, {-1, 0}, {-1,1}, {0,-1}, {0,1}, {1, -1}, {1,0}, {1,1}};
+    static int [][] dirs4 = {{-1,0}, {0, 1}, {1,0}, {0, -1}};
+
     public static void main(String[] args) throws Exception{
         input();
         for(int i = 1; i <= M; i++){
-            sout(i+" th turn ...");
-            moveRoudole();
-            // moveSantas();
-            if(allSantaAreFail()){
-                break;
+            // 루돌프 1번 move
+            roudolfMove();
+            
+            // 순서대로 산타 move
+            for(int num = 1; num <=P; num++){
+                santaMove(num);
+                
             }
-            addOneScoreNotFailedSantas();
-            reduceSleep();
-        }  
-        
+            // if(산타 모두 탈락) -> 게임 종료
+            if(allSantaFail()) break;
+            // 탈락하지 않은 산타들에 대해 1점 점수 부여
+            addOneScoreNotFailSantas();
+            // 기절한 산타들은 카운트 -1
+            reduceSleepCount();
+        }
+        printEachSantaScores();
+    }
+    static boolean existSameArea(Santa santa, int rouR, int rouC){
+        return santa.r == rouR && santa.c == rouC;
     }
 
-    static void moveRoudole(){
+    static void crash(int isRoudolf, int dir, Santa santa, int rouR, int rouC){
+        // sout("crash ..");
+        // sout("roudolf "+ rouR+ " , "+rouC);
+        // printArr(santaNumMap, "santaNumMap");
+        int x = rouR;
+        int y = rouC;
+        int nextR = -1;
+        int nextC = -1;
+        if(isRoudolf == 1){
+            santa.score += C;
+            nextR = x + (dirs8[dir][0]*C);
+            nextC = y + (dirs8[dir][1]*C);
+            
+        }else{
+            santa.score += D;
+            dir = dir < 2 ? dir+2 : dir -2;
+            nextR = x + (dirs4[dir][0]*D);
+            nextC = y + (dirs4[dir][1]*D);
+        }
+        // sout("next 위치 "+ nextR+ " , "+nextC);
+        if(!inArea(nextR, nextC)) {
+            santaFail(santa);
+            return;
+        }
+        // 다른 산타가 있다면
+        if(santaNumMap[nextR][nextC] > 0){ // 상호작용
+            santaNumMap[x][y] = 0;
+            interact(isRoudolf, dir, nextR, nextC, santa); // 산타를 다음 방향에 착지시킨다.
+            
+        }else{ // 없다면
+            moveSantaInSantaNumMap(santa.r, santa.c, nextR, nextC);
+            santa.r = nextR;
+            santa.c = nextC;
+        }
 
-        Santa targetSanta = null;
+        // 기절
+        santa.sleep +=2;
+        
+    }
+    static void interact(int isRoudolf, int dir, int r, int c, Santa santa){
+        // santa를 해당 위치에 착지 시킨다
+        if(!inArea(r, c)){
+            santaFail(santa);
+        }
+        if(santaNumMap[r][c] > 0){
+            int num = santaNumMap[r][c];
+            int nx = r;
+            int ny = c;
+            if(isRoudolf == 1){
+                nx += dirs8[dir][0];
+                ny += dirs8[dir][1];
+            }else{
+                nx += dirs4[dir][0];
+                ny += dirs4[dir][1];
+            }
+   
+            interact(isRoudolf, dir, nx, ny, santaMap.get(num));
+
+        }
+        santaNumMap[r][c] = santa.num;
+        santa.r = r;
+        santa.c = c;
+
+    }
+    static void printArr(int [][] map , String msg){
+        sout(" --------- "+ msg + " -----------");
+        for(int i = 0;i < map.length; i++){
+            sout(Arrays.toString(map[i]));
+        }
+        sout(" --------- -----------");
+    }
+    static void santaFail(Santa santa){
+        santa.is_out = true;
+        santaNumMap[santa.r][santa.c] = 0;
+    }
+    static void moveSantaInSantaNumMap(int fx, int fy, int ex, int ey){
+        santaNumMap[ex][ey] = santaNumMap[fx][fy];
+        santaNumMap[fx][fy] = 0;
+    }
+
+    static void santaMove(int num){
+        // sout(" santan move... ");
+        Santa santa = santaMap.get(num);
+        if(santa.sleep > 0 || santa.is_out) return;
+        // sout(" before "+ santaMap.get(num).toString());
+        int oriDist = getDist(santa.r, santa.c, rouR , rouC);
         int minDist = Integer.MAX_VALUE;
-        // 우선 순위 산타 선정
-        for(int num = 1; num <= P; num++){
-            Santa santa = santaHMap.get(num);
-            int dist = getDist(santa.r, santa.c, rouR, rouC);
-            if( dist < minDist || (dist == minDist && santa.compareTo(targetSanta) < 0)){
+        int minDir = -1;
+        // 루돌프와 거리가 가장 가까워지는 방향으로 1칸 이동
+        for(int d = 0; d < 4; d++){
+            int nx = santa.r + dirs4[d][0];
+            int ny = santa.c + dirs4[d][1];
+            if(!inArea(nx, ny) || santaNumMap[nx][ny] > 0) continue;
+            int dist = getDist(nx, ny, rouR , rouC);
+            if( dist >= oriDist ) continue;
+            if( dist < minDist ){
+                minDist = dist;
+                minDir = d;
+            }
+        }
+        // 그러한 방향이 있을 경우에만 이동
+        if(minDir != -1){
+            assert santaNumMap[santa.r][santa.c] == 0;
+            int nr = santa.r + dirs4[minDir][0];
+            int nc = santa.c + dirs4[minDir][1];
+            santaNumMap[santa.r][santa.c] = 0;
+            santa.r = nr;
+            santa.c = nc;
+            santaNumMap[santa.r][santa.c] = santa.num;
+            // sout(" after "+ santaMap.get(num).toString());   
+            if(existSameArea(santa, rouR, rouC)) crash(0, minDir, santa, rouR, rouC);
+        }
+        
+
+    }
+    
+    static void roudolfMove(){
+        // 가장 높은 우선순위 산타를 정한다.
+        Santa targetSanta = null;
+        int minDist= Integer.MAX_VALUE;
+
+        for(int num = 1; num <=P; num++){
+            Santa santa = santaMap.get(num);
+            if(santa.is_out) continue;
+            int dist = getDist(rouR, rouC, santa.r, santa.c);
+            if(isPrior(dist, santa, minDist, targetSanta)){
                 targetSanta = santa;
                 minDist = dist;
             }
         }
 
-        sout("우선순위 산타: "+ targetSanta.toString());
-        // 우선순위 산타에 대해 8방향 중 가장 가까워지는 방향 선택 후, 1칸 이동
-        for(int d = 0; d < 8; d++){
-            int nextRouR = rouR + rouDirs[d][0];
-            int nextRouC = rouC + rouDirs[d][1];
-            int dist = 
+        // sout("가장 높은 우선 순위 산타 : " + targetSanta.num + " " + targetSanta.r + " , " + targetSanta.c);
+
+        // 8 방향 중 가장 가까워지는 방향을 찾는다.
+        int closestDir = findCloesetDirIn8Dirs(rouR, rouC, targetSanta.r, targetSanta.c);
+        // sout(" 찾은 방향 "+ closestDir+ " 현재 루돌프 위치 : "+ rouR+ " , "+ rouC);
+
+        if(closestDir != -1){
+            // 해당 방향으로 이동
+            rouR += dirs8[closestDir][0];
+            rouC += dirs8[closestDir][1];
+            if(!inArea(rouR, rouC)){
+                sout(" 루돌프 이동 방향 이상");
+                System.exit(0);
+            }
+            if(existSameArea(targetSanta, rouR, rouC)) crash(1, closestDir, targetSanta, rouR, rouC);
         }
-        int minDir = bfsFindMinDir(rouR, rouC, targetSanta.r, targetSanta.c, rouDirs);
-        rouR += rouDirs[minDir][0];
-        rouC += rouDirs[minDir][1];
-
-        sout("선정된 루돌프 이동 위치 "+ rouR+" , "+ rouC+ " minDir = "+ minDir);
-
-        // if( 산타와 루돌프 같은 칸) -> 충돌
-    }
-    
-
-    static void moveSantas(){
         
     }
-    
-    
-
-
-
-
-
-
-
-    static boolean inArea(int x, int y){
-        return x > 0 && y > 0 && x <=N && y <=N;
+    static boolean inArea(int r, int c){
+        return r > 0 && c > 0 && r <=N && c <=N;
     }
+    static int findCloesetDirIn8Dirs(int r, int c, int targetR, int targetC){
+        if( targetR < r ){ // r < 0
+            if( targetC < c ){ //c < 0
+                return 0;
+            }else if (targetC - c == 0){
+                return 1;
+            }else{
+                return 2;
+            }
+        }else if(targetR == r){ // r == 0
+            if( targetC < c ){ //c < 0
+                return 3;
+            }else if (targetC == c){
+                return -1;
+            }else{
+                return 4;
+            }
+        }else{ //r < 0
+            if( targetC < c){ //c > 0
+                return 5;
+            }else if (targetC == c ){
+                return 6;
+            }else{
+                return 7;
+            }
 
-
-    static void reduceSleep(){
-        for(Map.Entry<Number, Santa> entry : santaHMap.entrySet()){
-            if(!entry.getValue().isSleeping()) entry.getValue().sleep -= 1;
         }
     }
 
-    static int getDist(int r1, int c1, int r2, int c2){
-        return (int) (Math.pow(r1-r2, 2) + Math.pow(c1-c2, 2));
+    static boolean isPrior(int dist, Santa santa, int minDist, Santa targetSanta){
+        if(dist == minDist) return santa.compareTo(targetSanta) < 0;
+        return dist < minDist;
     }
 
-    static void addOneScoreNotFailedSantas(){
-        for(Map.Entry<Number, Santa> entry : santaHMap.entrySet()){
-            if(!entry.getValue().isFail()) entry.getValue().score += 1;
+    static int getDist(int x1, int y1, int x2, int y2){
+        return (int) (Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2)); 
+    }
+
+    static void reduceSleepCount(){
+        for(Map.Entry<Integer, Santa> entry : santaMap.entrySet()) {
+            Santa santa = entry.getValue();
+            if(santa.sleep > 0) santa.sleep -= 1;
         }
     }
-    static boolean allSantaAreFail(){
-        for(Map.Entry<Number, Santa> entry : santaHMap.entrySet()){
-            if(!entry.getValue().isFail()) return false;
+    static void addOneScoreNotFailSantas(){
+        for(Map.Entry<Integer, Santa> entry : santaMap.entrySet()) {
+            Santa santa = entry.getValue();
+            if(!santa.is_out) santa.score += 1;
+        }
+    }
+
+    static boolean allSantaFail(){
+        for(Map.Entry<Integer, Santa> entry : santaMap.entrySet()) {
+            if(!entry.getValue().is_out) return false;
         }
         return true;
     }
 
-
+    static void printEachSantaScores(){
+        for(Map.Entry<Integer, Santa> entry : santaMap.entrySet()) {
+            sb.append(entry.getValue().score).append(" ");
+        }
+        sout(sb.toString());
+    }
     static void input() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         N = toInt(st.nextToken());
         M = toInt(st.nextToken());
         P = toInt(st.nextToken());
         C = toInt(st.nextToken());
         D = toInt(st.nextToken());
+
+        santaNumMap = new int[N+1][N+1];
+
         st = new StringTokenizer(br.readLine());
         rouR = toInt(st.nextToken());
         rouC = toInt(st.nextToken());
-        for(int i = 0; i <P; i++){
+
+        for(int i = 1; i <= P; i++){
             st = new StringTokenizer(br.readLine());
             int num = toInt(st.nextToken());
             int r = toInt(st.nextToken());
             int c = toInt(st.nextToken());
-            santaHMap.put(num, new Santa(num, r,c));
+            santaMap.put(num, new Santa(num , r, c));
+            santaNumMap[r][c]  = num;
         }
-        
     }
-
     static int toInt(String s){
         return Integer.parseInt(s);
     }
-
     static void sout(String s){
         System.out.println(s);
     }
-    
+    static void sout(int s){
+        System.out.println(s);
+    }
+
 }
