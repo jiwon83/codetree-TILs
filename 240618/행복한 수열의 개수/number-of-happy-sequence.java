adc0edf4a5291517477e5dd4835a6a2f285142ae
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-06-18 08:08:41 +0900

    Title: 행복한 수열의 개수, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240618/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240618/행복한 수열의 개수/number-of-happy-sequence.java
@@ -22,32 +22,32 @@ public class Main {
     }
     static int solution(int [][] arr, int N, int M){
         int answer = 0;
+        int [] array = new int[N];
+        // row
         for(int i = 0; i < N; i++){
-            if(sequentialCount(arr, i, 1, N, M)) answer += 1;
-            if(sequentialCount(arr, i, 0, N, M)) answer += 1;
+            for(int j = 0; j < N; j++){
+                array[j] = arr[j][i];
+            }
+            if(sequentialCount(array , N, M)) answer += 1;
+        }
+        //col
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                array[j] = arr[i][j];
+            }
+            if(sequentialCount(array , N, M)) answer += 1;
         }
         return answer;
     }
-    static boolean sequentialCount(int [][] arr, int idx, int isRow, int N, int M){
-        // System.out.println(" idx = "+ idx + " isRow = "+ isRow);
-        int number = -1;
-        int count = 0;
-        for(int j = 0; j < N; j++){
-            if(isRow == 1){
-                if(number == arr[j][idx]) count+=1;
-                else {
-                    number = arr[j][idx];
-                    count = 1;
-                }
-            }else{
-                if(number == arr[idx][j]) count+=1;
-                else {
-                    number = arr[idx][j];
-                    count = 1;
-                }
-            }
-            if(count >= M) return true;
+
+    static boolean sequentialCount(int [] arr, int N, int M){
+        int maxCount = 1;
+        int sequenceCnt = 1;
+        for(int j = 1; j < N; j++){
+            if(arr[j-1] == arr[j]) sequenceCnt+=1;
+            else sequenceCnt = 1;
+            maxCount = Math.max(maxCount, sequenceCnt);
         }
-        return false;
+        return sequenceCnt >= M;
     }
 }
