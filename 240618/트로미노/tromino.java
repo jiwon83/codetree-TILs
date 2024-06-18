7f118448718ee1fbff25deaefcd6760510b3085d
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-06-18 09:35:48 +0900

    Title: 트로미노, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240618/트로미노/tromino.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int [][] block1,block2, map;
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j <m; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 여기에 코드를 작성해주세요.
+        initBlocks();
+        System.out.println(solution(n,  m));
+
+    }
+    static void initBlocks(){
+        block1 = new int[][]{
+            {1, 0},
+            {1, 1}
+        };
+        block2 = new int[][]{
+            {0,0,0},
+            {1,1,1},
+            {0,0,0}
+        };
+    }
+    static int solution(int N, int M){
+        int maxSum = 0;
+        //block 1
+        for(int r = 0; r < 4; r++){ // 총 4번의 회전
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j <M; j++){
+                    if(!inArea(i, j, block1.length, N, M)) continue;
+                    maxSum = Math.max(maxSum, getSum(i, j, block1, map));
+                }
+            }
+            block1 = roatate90(block1);
+        }
+
+        //block2
+        for(int r = 0; r < 4; r++){ // 총 4번의 회전
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j <M; j++){
+                    if(!inArea(i, j, block2.length, N, M)) continue;
+                    maxSum = Math.max(maxSum, getSum(i, j, block2, map));
+                }
+            }
+            block2 = roatate90(block2);
+        }
+        return maxSum;
+
+    }
+    static int [][] roatate90(int [][] arr){
+        
+        int len = arr.length;
+        int [][] tmp = new int[len][len];
+        for(int r = 0; r < len; r++){
+            for(int c = 0; c < len; c++){
+                int nr = c;
+                int nc = len - 1 - r;
+                tmp[nr][nc] = arr[r][c];
+            }   
+        }
+        return tmp;
+    }
+    static int getSum(int sr, int sc, int [][] block, int [][] map){
+        int sum = 0;
+        int len = block.length;
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len; j++){
+                int ni = sr + i;
+                int nj = sc + j;
+                if(block[i][j] > 0){
+                    sum += map[ni][nj];
+                }
+            }
+        }
+        return sum;
+    }
+    static boolean inArea(int sr, int sc, int len, int N, int M){
+        return sr + len - 1 < N && sc + len - 1 < M;
+    }
+}
