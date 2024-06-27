3d59035834564e55a1592b12d7cabef529c064a8
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-06-27 10:42:15 +0900

    Title: 외판원 순회, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240627/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int [][] costs;
+    static int N;
+    static int [] ch;
+    static int minCost = Integer.MAX_VALUE;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    
+    public static void main(String[] args) throws Exception{
+        // 여기에 코드를 작성해주세요.
+        N = Integer.parseInt(br.readLine());
+        costs = new int[N+1][N+1];
+        ch = new int [N+1];
+        for(int i = 1; i <= N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <=N; j++){
+                costs[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int st = 1; st <=N; st++){
+            ch[st] = 1;
+            recurVisit(1, 0, st, st);
+            ch[st] = 0;
+        }
+        
+        System.out.println(minCost);
+    }
+    private static void recurVisit(int k, int cost, int pre, int start){
+        if(k == N){
+            minCost = Math.min(minCost, cost + costs[pre][start]);
+            return;
+        }
+        for(int i = 1; i <= N; i++){
+            if(ch[i] == 1) continue;
+            ch[i] = 1;
+            recurVisit(k+1, cost + costs[pre][i], i, start);
+            ch[i] = 0;
+        }
+    }
+}
