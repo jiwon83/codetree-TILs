e352a07e48d5dd3d5f3c22e7938bec96b0848926
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-07-01 09:08:21 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 144ms, Memory: 26MB, Status: Passed - CodeTree

--- 240701/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 240701/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -1,5 +1,9 @@
 import java.util.*;
 import java.io.*;
+/*
+배운점 : 현재 MAX+n 이 10 ^ 6으로 여기서 /2가 무조건 이득이지만, 
+나우어 떨어지지 않을 경우 MAX_n + 여러번이 더 유리할 수도 있으므로 간편하게 N * 2를 가능한 숫자의 최댓값으로 잡으면 될 것 같다. 
+*/
 public class Main {
     static class Info{
         int num, cnt;
