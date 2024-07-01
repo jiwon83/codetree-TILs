c437ead7e192cb1ab31ad81d6691cb8397b151cc
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-07-01 09:09:33 +0900

    Title: k개의 벽 없애기, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240701/k개의 벽 없애기/remove-k-walls.java
@@ -0,0 +1,9 @@
+public class Main {
+    /* 
+    두가지 방법 -> 일단 bfs, 큐의 원소에 벽 부신 횟수를 넣고 이것으로 방문처리
+    -> 또는, 아예 부실 벽을 조합으로 생성하고 bfs
+    */ 
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    }
+}
