f86c32c457658107c4ed2b6311639be77c9c3f1f
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-07-04 11:32:14 +0900

    Title: 사각형 채우기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240704/사각형 채우기 3/rectangle-fill-3.java
+++ 240704/사각형 채우기 3/rectangle-fill-3.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-
+        int mod = 1000000007
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         
@@ -12,7 +12,7 @@ public class Main {
         dp[2] = 7;
         dp[3] = 22;
         for(int i = 4; i <= N; i++){
-            dp[i] = dp[i-1] * 2 + dp[i-2] * 3 + dp[i-3] * 2 + 2;
+            dp[i] = (dp[i-1] * 2 + dp[i-2] * 3 + dp[i-3] * 2 + 2) % mod;
         }
         System.out.println(dp[N]);
 
