98a7893da973184c57201806221cfe47a8e4ceb8
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-09-01 17:18:30 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 114ms, Memory: 8MB, Status: Passed - CodeTree

--- 240901/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
+++ 240901/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
@@ -2,43 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static long M;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    
-    public static void main(String[] args) throws Exception {
-        // 여기에 코드를 작성해주세요.
-        M = Long.parseLong(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        long a = Long.parseLong(st.nextToken());
-        long b = Long.parseLong(st.nextToken());
+   static long M;
+   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   static StringTokenizer st;
 
-        int minCnt = M + 1;
-        int maxCnt = -1;
+   public static void main(String[] args) throws Exception {
+      // 여기에 코드를 작성해주세요.
+      M = Long.parseLong(br.readLine());
+      st = new StringTokenizer(br.readLine());
+      long a = Long.parseLong(st.nextToken());
+      long b = Long.parseLong(st.nextToken());
 
-        for(long x = a; x <=b; x++){
-            int cnt = cntOfBinarySearch(x);
-            minCnt = Math.min(minCnt, cnt);
-            maxCnt = Math.max(maxCnt, cnt);
-        }
-        System.out.println(minCnt + " "+ maxCnt);
-    }
+      int minCnt = Integer.MAX_VALUE;
+      int maxCnt = -1;
 
-    static int cntOfBinarySearch(long target){
-        int cnt = 0;
-        long left = 1, right = M;
-        while(left <= right){
-            cnt += 1;
-            long mid = (left + right) / 2;
-            if(mid == target) return cnt;
-            if(mid > target){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
-        }
-        return -1; // 이럴 가능성은 없다.
-    }
+      for(long x = a; x <=b; x++){
+         int cnt = cntOfBinarySearch(x);
+         minCnt = Math.min(minCnt, cnt);
+         maxCnt = Math.max(maxCnt, cnt);
+      }
+      System.out.println(minCnt + " "+ maxCnt);
+   }
+
+   static int cntOfBinarySearch(long target){
+      int cnt = 0;
+      long left = 1, right = M;
+      while(left <= right){
+         cnt += 1;
+         long mid = (left + right) / 2;
+         if(mid == target) return cnt;
+         if(mid > target){
+            right = mid - 1;
+         }else{
+            left = mid + 1;
+         }
+      }
+      return -1; // 이럴 가능성은 없다.
+   }
 
 
 }
