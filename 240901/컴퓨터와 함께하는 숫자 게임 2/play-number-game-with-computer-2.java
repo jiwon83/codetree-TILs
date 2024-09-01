65a91532d679ad421a8bd26e76f7ef32a0a83a53
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-09-01 17:12:17 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240901/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int minCnt = M + 1;
+        int maxCnt = -1;
+
+        for(int x = a; x <=b; x++){
+            int cnt = cntOfBinarySearch(x);
+            minCnt = Math.min(minCnt, cnt);
+            maxCnt = Math.max(maxCnt, cnt);
+        }
+        System.out.println(minCnt + " "+ maxCnt);
+    }
+
+    static int cntOfBinarySearch(int target){
+        int cnt = 0;
+        int left = 1, right = M;
+        while(left <= right){
+            cnt += 1;
+            int mid = (left + right) / 2;
+            if(mid == target) return cnt;
+            if(mid > target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return -1; // 이럴 가능성은 없다.
+    }
+
+
+}
